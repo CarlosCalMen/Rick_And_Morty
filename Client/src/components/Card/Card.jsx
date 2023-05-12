61df4ddb0a41318c6f3e3a91c002e437f6ac4669
CarlosCalMen <113527119+CarlosCalMen@users.noreply.github.com>
2023-05-12 14:03:06 -0500

    con express, falta css

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {Link,useLocation} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { useState,useEffect } from 'react';
 import { addFav,removeFav } from '../../redux/actions';
@@ -17,7 +17,7 @@ const Card=({id,name,status,species,gender,origin,image,onClose,addFav,removeFav
          addFav({id,name,status,species,gender,origin,image,onClose});
       }
    };
-
+   
    useEffect(() => {
       myFavorites.forEach((char) => {
          if (char.id === id) {
@@ -25,18 +25,21 @@ const Card=({id,name,status,species,gender,origin,image,onClose,addFav,removeFav
          }
       });
    }, [myFavorites]);
-
+   let {pathname} = useLocation();
+   
    return (
       <div className={style.carta}>
          <div className={style.contenedorBoton}>
          {
-            isFav ? (
-            <button onClick={handleFavorite}>❤️</button>
-            ) : (
-            <button onClick={handleFavorite}>🤍</button>
-            )
+            isFav 
+            ? (<button onClick={handleFavorite}>❤️</button>)
+            : (<button onClick={handleFavorite}>🤍</button>)
+         };
+         {
+            pathname!=='/favorites'
+            ?(<button className={style.boton}onClick={()=>onClose(id)}>X</button>)
+            :null
          }
-         <button className={style.boton}onClick={()=>onClose(id)}>X</button>
          </div>
          <img src={image} alt='' /> 
          {/*<h2>{id}</h2>*/}
--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -9,11 +9,13 @@ const Nav=({onSearch,myFavorites})=>{
     return (
         <div className={style.contenedor}>
             <div className={style.botones}>
-            <Link to='/about'><button>About</button></Link> 
-            <Link to='/home'> <button>Home</button></Link> 
-            <Link to='/favorites'><button>Favorites</button></Link>
+                <Link to='/about'><button>About</button></Link> 
+                <Link to='/home'> <button>Home</button></Link> 
+                <Link to='/favorites'><button>Favorites</button></Link>
             </div>
-            <SearchBar onSearch={onSearch}/>
+            <div>
+                <SearchBar onSearch={onSearch}/>
+            </div>
         </div>
     )
 
